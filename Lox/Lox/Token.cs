046b7a993118f7de547d62f8707152cdb73b51ca
Darki002 <113591988+Darki002@users.noreply.github.com>
2024-08-19 16:33:15 +0200

    Update Token.cs

--- Lox/Lox/Token.cs
+++ Lox/Lox/Token.cs
@@ -3,8 +3,8 @@ namespace Lox;
 public class Token(TokenType tokenType, string lexeme, object literal, int line)
 {
     public readonly TokenType TokenType = tokenType;
-    public readonly string Lexeme = lexeme;
-    public readonly object Literal = literal;
+    public readonly string Lexeme = lexeme; // The string representing this token, ex. "var"
+    public readonly object Literal = literal; // Parsed value so we can use the value in the correct type later on
     public readonly int Line = line;
 
 
