50e4b2a88b71bcc8937d14cc995627bc640f457c
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-02-02 12:42:27 +0100

    Create Resolver.cs

---
+++ JLox/Lox/Resolver.cs
@@ -0,0 +1,127 @@
+﻿namespace Lox;
+
+public class Resolver(Interpreter interpreter) : Stmt.IVisitor<Void?>, Expr.IVisitor<Void?>
+{
+    private readonly Interpreter interpreter = interpreter;
+    private readonly Stack<Dictionary<string, bool>> scopes = new Stack<Dictionary<string, bool>>();
+
+    public Void? VisitBlockStmt(Stmt.Block stmt)
+    {
+        BeginScope();
+        Resolve(stmt.Statements);
+        EndScope();
+        return null;
+    }
+
+    public Void? VisitExpressionStmt(Stmt.Expression stmt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitFunctionStmt(Stmt.Function stmt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitIfStmt(Stmt.If stmt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitPrintStmt(Stmt.Print stmt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitReturnStmt(Stmt.Return stmt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitVarStmt(Stmt.Var stmt)
+    {
+        Declare(stmt.Name);
+        if (stmt.Initializer != null)
+        {
+            Resolve(stmt.Initializer);
+        }
+
+        Define(stmt.Name);
+        return null;
+    }
+
+    public Void? VisitWhileStmt(Stmt.While stmt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitBreakStmt(Stmt.Break stmt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitAssignExpr(Expr.Assign expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitBinaryExpr(Expr.Binary expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitGroupingExpr(Expr.Grouping expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitLiteralExpr(Expr.Literal expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitLogicalExpr(Expr.Logical expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitUnaryExpr(Expr.Unary expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitCallExpr(Expr.Call expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitVariableExpr(Expr.Variable expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Void? VisitFunctionExpr(Expr.Function expr)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void BeginScope()
+    {
+        scopes.Push(new Dictionary<string, bool>());
+    }
+    
+    private void EndScope() {
+        scopes.Pop();
+    }
+    
+    private void Resolve(List<Stmt> statements) {
+        foreach (var statement in statements)
+        {
+            Resolve(statement);
+        }
+    }
+
+    private void Resolve(Stmt stmt) => stmt.Accept(this);
+    
+    private void Resolve(Expr expr) => expr.Accept(this);
+}
