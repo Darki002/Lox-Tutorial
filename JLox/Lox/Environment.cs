b8c08672532e82d6bea82d1786c70edd0ca6e8fc
Darki002 <113591988+Darki002@users.noreply.github.com>
2025-03-16 16:04:35 +0100

    Revert "indexing"
    This reverts commit 97c775322b1363bbdbe0b866934d5eb8a944ddbc.

--- JLox/Lox/Environment.cs
+++ JLox/Lox/Environment.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Specialized;
-using Lox.Errors;
+﻿using Lox.Errors;
 
 namespace Lox;
 
@@ -7,16 +6,22 @@ public class Environment(Environment? enclosing = null)
 {
     private readonly Environment? enclosing = enclosing;
     
-    private readonly OrderedDictionary variables = new();
+    private readonly Dictionary<string, object?> values = new();
 
     public void Define(string name, object? value)
     {
-        variables[name] = value;
+        if (!values.TryAdd(name, value))
+        {
+            values[name] = value;
+        }
     }
 
     public object? Get(Token name)
     {
-        if (variables.Contains(name.Lexeme)) return variables[name.Lexeme];
+        if (values.TryGetValue(name.Lexeme, out var value))
+        {
+            return value;
+        }
 
         if (enclosing is not null) return enclosing.Get(name);
 
@@ -25,14 +30,14 @@ public class Environment(Environment? enclosing = null)
     
     public object? GetAt(int distance, int index)
     {
-        return Ancestor(distance)?.variables[index];
+        return Ancestor(distance)?.values.ElementAt(index);
     }
 
     public void Assign(Token name, object? value)
     {
-        if (variables.Contains(name.Lexeme))
+        if (values.ContainsKey(name.Lexeme))
         {
-            variables[name.Lexeme] = value;
+            values[name.Lexeme] = value;
             return;
         }
         
@@ -45,10 +50,10 @@ public class Environment(Environment? enclosing = null)
         throw new RuntimeError(name, $"Undefined variable '{name.Lexeme}'.");
     }
 
-    public void AssignAt(int distance, int index, object? value)
+    public void AssignAt(int distance, Token name, object? value)
     {
         var ancestor = Ancestor(distance);
-        ancestor!.variables[index] = value;
+        ancestor!.values[name.Lexeme] = value;
     }
     
     private Environment? Ancestor(int distance)
