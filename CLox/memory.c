b5b8f4f21fe2d589ad92b4c2636d61e32b9fe76f
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-07-23 14:10:00 +0200

    memory managment for chunks

---
+++ CLox/memory.c
@@ -0,0 +1,21 @@
+#include "memory.h"
+
+#include <stdlib.h>
+
+/*
+ oldSize | newSize | Operation
+ 0 | Non-zero | Allocate new block
+ Non-zero | 0 | Free allocation
+ Non-Zero | Smaller than oldSize | Shrink existing allocation
+ Non-zero | Larger than oldSize | Grow existing allocation.
+ */
+void* reallocate(void* pointer, size_t oldSize, const size_t newSize) {
+    if (newSize == 0) {
+        free(pointer);
+        return nullptr;
+    }
+
+    void* result = realloc(pointer, newSize);
+    if (result == nullptr) exit(1);
+    return result;
+}
