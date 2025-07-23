48214442bbbd8908ee7b3b78257f4942488a1dae
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-07-23 13:27:15 +0200

    calc grow capacity

--- CLox/chunk.c
+++ CLox/chunk.c
@@ -1,17 +1,18 @@
 #include "chunk.h"
 
+#include "memory.h"
+
 void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = nullptr;
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, const uint8_t byte) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-
     }
 
     chunk->code[chunk->count] = byte;
