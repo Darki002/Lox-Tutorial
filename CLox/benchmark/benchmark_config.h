256ba5f02f68096e091ce0b39f12c4a5ad165815
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-09-04 15:33:33 +0200

    benchmark delete

--- CLox/benchmark/benchmark_config.h
+++ CLox/benchmark/benchmark_config.h
@@ -1,17 +1,17 @@
 #ifndef BENCHMARK_CONFIG_H
 #define BENCHMARK_CONFIG_H
 
-#include "hashtable/find.h"
+#include "hashtable/delete.h"
 
-#define benchmark runFind
-#define setUpBenchmark setUpFind
-#define freeBenchmark freeFind
+#define benchmark runDelete
+#define setUpBenchmark setUpDelete
+#define freeBenchmark freeDelete
 
 #define warmups 3
 #define iterations 100
 
 const char* basePath = "C:/Development/Private/Lox-Tutorial/CLox/benchmark";
-const char* resultFile = "hashtable/find.txt";
+const char* resultFile = "hashtable/delete.txt";
 const char* benchmarkName = "Orig";
 
 #endif //BENCHMARK_CONFIG_H
---
+++ CLox/benchmark/hashtable/delete.h
@@ -0,0 +1,10 @@
+#ifndef DELETE_H
+#define DELETE_H
+
+#define COUNT 1000
+
+void setUpDelete();
+void freeDelete();
+void runDelete();
+
+#endif //DELETE_H
