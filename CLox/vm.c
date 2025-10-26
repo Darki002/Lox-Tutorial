5e0b5cc5980b128f210b77629a41ce5ea5681317
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2025-10-26 14:29:25 +0100

    extend err method

--- CLox/vm.c
+++ CLox/vm.c
@@ -45,9 +45,34 @@ static bool readNative(const int argCount, Value* args) {
     return true;
 }
 
-static bool errNative(int _, Value* args) {
-    args[-1] = OBJ_VAL(copyString("Error!", 6));
-    return false;
+static bool errNative(const int argCount, Value* args) {
+    switch (argCount) {
+        case 0: {
+            args[-1] = OBJ_VAL(copyString("Error!", 6));
+            return false;
+        }
+        case 1: {
+            if (!IS_STRING(args[0])) {
+                args[-1] = OBJ_VAL(copyString("Expected a string as argument.", 30));
+                return false;
+            }
+
+            const ObjString* message = AS_STRING(args[0]);
+            const int len = 7 + message->length;
+
+            ObjString* errMessage = allocateString(len);
+            memcpy(errMessage->chars, "Error: ", 7);
+            memcpy(errMessage->chars + 7, message->chars, message->length);
+            errMessage->chars[len] = '\0';
+
+            args[-1] = OBJ_VAL(errMessage);
+            return false;
+        }
+        default: {
+            args[-1] = OBJ_VAL(copyString("Unexpected amount of arguments.", 31));
+            return false;
+        }
+    }
 }
 
 static void resetStack() {
