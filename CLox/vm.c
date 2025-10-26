2d96d52f21a21bc91905dd5a70485a584155f19c
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2025-10-26 14:15:21 +0100

    prompt argument for read

--- CLox/vm.c
+++ CLox/vm.c
@@ -20,7 +20,20 @@ static bool clockNative(int _, Value* args) {
     return true;
 }
 
-static bool readNative(int _, Value* args) {
+static bool readNative(const int argCount, Value* args) {
+    if (argCount > 1) {
+        args[-1] = OBJ_VAL(copyString("Too many arguments.", 19));
+        return false;
+    }
+    if (argCount == 1) {
+        if (!IS_STRING(args[0])) {
+            args[-1] = OBJ_VAL(copyString("Expected a string as argument.", 30));
+            return false;
+        }
+        const int len = AS_STRING(args[0])->length;
+        printf("%.*s", len, AS_CSTRING(args[0]));
+    }
+
     const ObjString* result = readLine(stdin);
 
     if (result == NULL) {
