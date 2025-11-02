ea2c0b3e40b92b83e7bda0385a5a8393e6a07b3e
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2025-11-02 15:41:38 +0100

    fix crossplatform sleep

--- CLox/stdlib/time.c
+++ CLox/stdlib/time.c
@@ -2,11 +2,27 @@
 #include "time.h"
 #include "../object.h"
 
-#include <unistd.h>
+#ifdef WIN32
+    #include "windows.h"
+#endif
 
 bool clockNative(int _, Value* args) {
     args[-1] = NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
     return true;
+}
+
+static void sleep_ms(int milliseconds)
+{
+    #ifdef WIN32
+        Sleep(milliseconds);
+    #elif _POSIX_C_SOURCE >= 199309L
+        struct timespec ts;
+        ts.tv_sec = milliseconds / 1000;
+        ts.tv_nsec = (milliseconds % 1000) * 1000000;
+        nanosleep(&ts, NULL);
+    #else
+        usleep(milliseconds * 1000);
+    #endif
 }
 
 bool sleepNative(const int argCount, Value* args) {
@@ -21,7 +37,7 @@ bool sleepNative(const int argCount, Value* args) {
         return false;
     }
 
-    sleep(AS_NUMBER(value));
+    sleep_ms(AS_NUMBER(value) * 1000);
     args[-1] = NIL_VAL;
     return true;
 }
